b96a7f7ad4abcf058ae6ce171eac302ea3942fc7
Jeff Matthews <jmatthew@adobe.com>
2023-09-05 13:20:33 -0500

    Added return

--- .github/scripts/test-metadata.js
+++ .github/scripts/test-metadata.js
@@ -3,24 +3,27 @@
 // Documentation for the actions/github-script:
 // https://github.com/actions/github-script#run-a-separate-file
 
-module.exports = testMetadata;
+module.exports = testMetadata
 
 const glob = require('glob')
-const path = 'src/pages'
-const markdownFiles = glob(path + '/**/*.md')
+const markdownFiles = await glob('**/*.md', {ignore: 'node_modules/**'})
 
 console.log('\x1b[35m ==== FRONTMATTER REPORT - STARTING ==================== \x1b[0m');
 console.info('\x1b[36m [cmd + click] on the file path to open \x1b[0m');
   
-markdownFiles.forEach(file => {
-  if (!file.description || !file.keywords || !file.title) {
-    console.log('\n\x1b[41mFail\x1b[0m - ' + file.fileAbsolutePath + ': ');
-    if (!file.title) console.error('\x1b[33m\tMissing Title \x1b[0m');
-    if (!file.description) console.error('\x1b[33m\tMissing Description \x1b[0m');
-    if (!file.keywords) console.warn('\x1b[33m\tMissing Keywords \x1b[0m');
-  } else {
-    console.log('\x1b[42mPass\x1b[0m - ' + file.fileAbsolutePath);
-    }
-  });
+function testMetadata() {
+    return [
+        markdownFiles.forEach(file => {
+            if (!file.description || !file.keywords || !file.title) {
+              console.log('\n\x1b[41mFail\x1b[0m - ' + file.fileAbsolutePath + ': ');
+              if (!file.title) console.error('\x1b[33m\tMissing Title \x1b[0m');
+              if (!file.description) console.error('\x1b[33m\tMissing Description \x1b[0m');
+              if (!file.keywords) console.warn('\x1b[33m\tMissing Keywords \x1b[0m');
+            } else {
+                console.log('\x1b[42mPass\x1b[0m - ' + file.fileAbsolutePath);
+                }
+            }),
+    ]
+}
 
 console.log('\x1b[35m ==== FRONTMATTER REPORT - FINISHED ==================== \x1b[0m');
