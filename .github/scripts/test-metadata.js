7a6d528ac9e8479ef79af24d2a9dfcf2800fbe18
Jeff Matthews <jmatthew@adobe.com>
2023-09-05 11:48:31 -0500

    COMDOX-345: Debug

--- .github/scripts/test-metadata.js
+++ .github/scripts/test-metadata.js
@@ -3,22 +3,20 @@
 // Documentation for the actions/github-script:
 // https://github.com/actions/github-script#run-a-separate-file
 
-  console.log('\x1b[35m ==== FRONTMATTER REPORT - STARTING ==================== \x1b[0m');
-  console.info('\x1b[36m [cmd + click] on the file path to open \x1b[0m');
-
-  function testMetadata(){
-    forEach(file => {
-        if (!file.description || !file.keywords || !file.title) {
-          console.log('\n\x1b[41mFail\x1b[0m - ' + file.fileAbsolutePath + ': ');
-          if (!file.title) console.error('\x1b[33m\tMissing Title \x1b[0m');
-          if (!file.description) console.error('\x1b[33m\tMissing Description \x1b[0m');
-          if (!file.keywords) console.warn('\x1b[33m\tMissing Keywords \x1b[0m');
-        } else {
-          console.log('\x1b[42mPass\x1b[0m - ' + file.fileAbsolutePath);
-          }
-      });
-    
-      console.log('\x1b[35m ==== FRONTMATTER REPORT - FINISHED ==================== \x1b[0m');
-  }
-
 module.exports = testMetadata;
+
+console.log('\x1b[35m ==== FRONTMATTER REPORT - STARTING ==================== \x1b[0m');
+console.info('\x1b[36m [cmd + click] on the file path to open \x1b[0m');
+
+testMetadata.forEach(file => {
+    if (!file.description || !file.keywords || !file.title) {
+    console.log('\n\x1b[41mFail\x1b[0m - ' + file.fileAbsolutePath + ': ');
+    if (!file.title) console.error('\x1b[33m\tMissing Title \x1b[0m');
+    if (!file.description) console.error('\x1b[33m\tMissing Description \x1b[0m');
+    if (!file.keywords) console.warn('\x1b[33m\tMissing Keywords \x1b[0m');
+    } else {
+    console.log('\x1b[42mPass\x1b[0m - ' + file.fileAbsolutePath);
+    }
+});
+
+console.log('\x1b[35m ==== FRONTMATTER REPORT - FINISHED ==================== \x1b[0m');
