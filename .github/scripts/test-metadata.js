3fc3817df78d215648d1630a859271caa13b5e0f
Jeff Matthews <jmatthew@adobe.com>
2023-09-05 12:34:32 -0500

    Try this

--- .github/scripts/test-metadata.js
+++ .github/scripts/test-metadata.js
@@ -5,13 +5,17 @@
 
 module.exports = testMetadata;
 
-const markdownFiles = require("../../src/pages");
+var glob = require('glob')
+  , path = require('path');
+
+glob.sync('./src/pages/**/*.md').forEach
 
 console.log('\x1b[35m ==== FRONTMATTER REPORT - STARTING ==================== \x1b[0m');
 console.info('\x1b[36m [cmd + click] on the file path to open \x1b[0m');
   
   function testMetadata(){
-    markdownFiles.forEach(file => {
+    glob.forEach(file => {
+        require(path.resolve(file));
         if (!file.description || !file.keywords || !file.title) {
           console.log('\n\x1b[41mFail\x1b[0m - ' + file.fileAbsolutePath + ': ');
           if (!file.title) console.error('\x1b[33m\tMissing Title \x1b[0m');
